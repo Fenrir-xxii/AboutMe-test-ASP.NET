bdc48c1b319b6e9cb6b19120cdbf83df98642e0f
Vasyl Bihan <bihanvasyl@gmail.com>
2023-09-14 23:40:32 +0300

    deleting tasks completed

--- WebApplication2-AboutMe/Controllers/TaskController.cs
+++ WebApplication2-AboutMe/Controllers/TaskController.cs
@@ -9,8 +9,7 @@ namespace WebApplication2_AboutMe.Controllers;
 public class TaskController : Controller
 {
     private readonly SiteContext _siteContext;
-    private readonly UserManager<User> _userManager;
-    private readonly int _userId;   
+    private readonly UserManager<User> _userManager; 
     public TaskController(SiteContext siteContext, UserManager<User> userManager)
     {
         _siteContext = siteContext;
@@ -36,9 +35,10 @@ public class TaskController : Controller
             return View(task);
         }
 
-        System.Security.Claims.ClaimsPrincipal currentUser = this.User;
+        //System.Security.Claims.ClaimsPrincipal currentUser = this.User;
+        //var currentUser = GetCurrentUser();
         var id = int.Parse(_userManager.GetUserId(User));
-        var dbUser = _siteContext.Users.FirstOrDefault(x => x.Id == _userId);
+        var dbUser = _siteContext.Users.FirstOrDefault(x => x.Id == id);
         task.User = dbUser;
 
         _siteContext.Add(task);
@@ -66,5 +66,47 @@ public class TaskController : Controller
 
     //    //return await System.Threading.Tasks.Task.Run(() => View("Index"));
     //    return RedirectToAction("Index");
-//}
+    //}
+    public async Task<User> GetCurrentUser()
+    {
+        return await _userManager.GetUserAsync(HttpContext.User);
+    }
+
+    [HttpPost]
+    public IActionResult CompleteTask([FromBody] int taskId)
+    {
+        var task = _siteContext.Tasks.First(x => x.Id == taskId);
+
+        task.IsCompleted = true;
+        _siteContext.SaveChanges();
+        return RedirectToAction("Index");
+    }
+    [HttpPost]
+    public IActionResult RestoreTask([FromBody] int taskId)
+    {
+        var task = _siteContext.Tasks.First(x => x.Id == taskId);
+
+        task.IsCompleted = false;
+        _siteContext.SaveChanges();
+        return RedirectToAction("Index");
+    }
+    [HttpPost]
+    public IActionResult DeleteTask([FromBody] int taskId)
+    {
+        var task = _siteContext.Tasks.First(x => x.Id == taskId);
+
+        _siteContext.Remove(task);
+        _siteContext.SaveChanges();
+        return RedirectToAction("Index");
+    }
+    [HttpPost]
+    public IActionResult DeleteAllCompleted([FromBody] string date)
+    {
+        var userId = int.Parse(_userManager.GetUserId(this.User));
+        var selectedDate = DateTime.Parse(date);
+        _siteContext.Tasks.RemoveRange(_siteContext.Tasks.Where(x => x.User.Id == userId).Where(x => x.Date == selectedDate).Where(x => x.IsCompleted==true));
+        _siteContext.SaveChanges();
+
+        return RedirectToAction("Index");
+    }
 }
--- WebApplication2-AboutMe/wwwroot/local/js/to-do-list.js
+++ WebApplication2-AboutMe/wwwroot/local/js/to-do-list.js
@@ -3,11 +3,14 @@
 $('#datepicker').datepicker({
     format: 'dd/mm/yyyy',
 });
+$("#datepicker").datepicker("setDate", new Date());
 
 let selectedDate = $('#datepicker').datepicker('getFormattedDate');
 let table = document.getElementById("tableBody");
+showTasks();
 
-$('#datepicker').on('changeDate', function () {
+$('#datepicker').on('changeDate', showTasks);
+function showTasks() {
     $('#my_hidden_input').val(
         selectedDate = $('#datepicker').datepicker('getFormattedDate')
     );
@@ -18,116 +21,103 @@ $('#datepicker').on('changeDate', function () {
         },
         body: JSON.stringify(selectedDate)
     }).then(r => r.json()).then(tasks => {
-        table.innerHTML = ""; 
+        table.innerHTML = "";
         tasks.forEach(t => {
             console.log(t);
             let row = table.insertRow();
             let checkbox = row.insertCell(0);
+            let taskId = t.id;
+            console.log("taskId", taskId);
+            console.log("checkbox", checkbox);
+
+            if (t.isCompleted) {
+                checkbox.innerHTML = `<input class="form-check-input" type = "checkbox" value = "" id = "flexCheckChecked" checked>`
+            } else {
+                checkbox.innerHTML = `<input class="form-check-input" type = "checkbox" value = "" id = "flexCheckDefault">`
+            }
+            checkbox.firstChild.addEventListener('change', function () {
+                console.log("inside checkbox", this);
+                if (this.checked) {
+                    fetch("/Task/CompleteTask", {
+                        method: "POST",
+                        headers: {
+                            'Content-Type': "application/json"
+                        },
+                        body: JSON.stringify(taskId)
+                    })
+
+                }
+                else {
+                    fetch("/Task/RestoreTask", {
+                        method: "POST",
+                        headers: {
+                            'Content-Type': "application/json"
+                        },
+                        body: JSON.stringify(taskId)
+                    })
+                }
+            })
             let title = row.insertCell(1);
             title.innerHTML = t.title;
+            title.addEventListener('dblclick', function () {
+                console.log("dblclick");
+                Swal.fire({
+                    input: 'textarea',
+                    inputLabel: 'New Title of Task',
+                    //inputPlaceholder: 'Type new title of this task...',
+                    //inputAttributes: {
+                    //    'aria-label': 'Type new title of this task'
+                    //},
+                    inputValue: t.title,
+                    showCancelButton: true
+                })
+
+            })
+
             let date = row.insertCell(2);
-            date.innerHTML = t.date;
+            //date.innerHTML = t.date.toString();
+            date.innerHTML = moment(t.date).format('DD.MM.YYYY')
             let del = row.insertCell(3);
+            del.innerHTML = `<button type="button" class="btn btn-danger">X</button>`
+            del.firstChild.addEventListener('click', function () {
+                fetch("/Task/DeleteTask/", {
+                    method: "POST",
+                    headers: {
+                        'Content-Type': "application/json"
+                    },
+                    body: JSON.stringify(taskId)
+                }).then(() => {
+                    showTasks();
+                })
+
+            })
+            let edit = row.insertCell(4);
+            edit.innerHTML = `<button type="button" class="btn btn-warning">E</button>`
+            edit.firstChild.addEventListener('click', function () {
+                //fetch("/Task/EditTask/", {
+                //    method: "POST",
+                //    headers: {
+                //        'Content-Type': "application/json"
+                //    },
+                //    body: JSON.stringify({
+                //        Title: taskTitle,
+                //        Date: taskDate,
+                //        IsCompleted: false
+                //    })
+                //}).then(() => {
+                //    showTasks();
+                //})
+
+            })
         })
     })
-    //fetch("/Task/GetTasks", {
-    //    method: "POST",
-    //    headers: {
-    //        'Content-Type': "application/json"
-    //    },
-    //    body: JSON.stringify(selectedDate)
-    //})
-    
-});
-
-
-
-//$('.datepicker').datepicker()
-//    .on('changeDate', getTasks);
-
-//function getTasks() {
-//    fetch("/Task/GetTasks", {
-//        method: "POST",
-//        headers: {
-//            'Content-Type': "application/json"
-//        },
-//        body: JSON.stringify(selectedDate)
-//    }).then(r => r.json()).then(tasks => {
-//        tasks.forEach(t => {
-//            console.log(t);
-//        })
-//    })
-//}
-
-
-//function addListeners() {
-//    var checkboxes = document.querySelectorAll("input[type=checkbox][name=settings]");
-//    var delButtons = document.querySelectorAll("button[name=delete]");
-//    var editableCol = document.getElementsByClassName("editable")
-
-//    checkboxes.forEach(function (checkbox, index) {
-//        checkbox.addEventListener('change', function () {
-//            var rowIndex = $('#table tr').index($(this).closest('tr'));
-//            if (this.checked) {
-//                completeTask(rowIndex - 1)
-//            }
-//            else {
-//                restoreTask(rowIndex - 1)
-//            }
-//        })
-//    });
-//    delButtons.forEach(function (button, index) {
-//        if (button.getAttribute('listener') !== 'true') {
-//            console.log(`setting event @ #${index}`)
-//            button.addEventListener('click', function () {
-//                var rowIndex = $('#table tr').index($(this).closest('tr'));
-//                console.log(rowIndex)
-//                deleteTask(rowIndex - 1)
-//            })
-//        }
-//        else {
-//            console.log(`event exist @ #${index}`)
-//        }
-//    })
-//    delButtons.forEach(function (button) {
-//        button.setAttribute('listener', 'true')
-//    })
-//    // console.log(editableCol)
-//    for (let title of editableCol) {
-//        title.addEventListener('click', function (e) {
-//            let el = e.target
-//            let content = el.innerText
-//            el.innerHTML = `<input type="text" class="editable-input" value="${content}">`
-
-//            el.querySelector("input").focus()
-//            //updateTasks()
-//            // console.log(title)
-
-//            Array.from(document.querySelectorAll("input.editable-input"))
-//                .forEach(inp => {
-//                    inp.addEventListener('blur', (ev) => {
-//                        let editable = ev.target.closest(".editable")
-//                        //console.log("blur")
-//                        if (editable !== null) {
-//                            ev.target.closest(".editable").innerHTML = ev.target.value
-//                            // console.log("editable if")
-//                            updateTasks()
-//                        }
-//                    })
-//                })
-//        })
-//    }
-
-//}
-
+}
 function AddTask() {
 
     let taskTitle = document.getElementById('taskTitle').value;
-    /* let taskDate = datePicker.getAttribute('data-date');*/
-    //let taskDate = $('#datepicker').datepicker('getFormattedDate');
     let taskDate = $('#datepicker').datepicker('getUTCDate');
 
-    console.log("datepicker", datePicker);
+    //console.log("datepicker", datePicker);
     console.log("taskTitle", taskTitle);
     console.log("taskDate", taskDate);
 
@@ -141,5 +131,53 @@ function AddTask() {
             Date: taskDate,
             IsCompleted: false
         })
+    }).then(() => {
+        //location.reload();
+        showTasks();
+        Swal.fire({
+            position: 'top-end',
+            icon: 'success',
+            title: 'Task removed successfully',
+            showConfirmButton: false,
+            timer: 1500
+        })
     })
 }
+
+
+function removeAllCompleted() {
+    Swal.fire({
+        title: 'Are you sure?',
+        html: "deleting all completed tasks",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#910a27',
+        cancelButtonColor: '#545253',
+        confirmButtonText: 'Yes, delete them',
+        cancelButtonText: 'No, keep tasks',
+        allowOutsideClick: false,
+    }).then((result) => {
+        if (result.isConfirmed) {
+            fetch("/Task/DeleteAllCompleted", {
+                method: "post",
+                headers: {
+                    'Content-Type': "application/json"
+                },
+                body: JSON.stringify(selectedDate)
+            }).then(() => {
+                //location.reload();
+                showTasks();
+                Swal.fire({
+                    position: 'top-end',
+                    icon: 'success',
+                    title: 'Completed tasks was removed',
+                    showConfirmButton: false,
+                    timer: 1500
+                })
+            })
+        }
+    });
+
+}
+
+
